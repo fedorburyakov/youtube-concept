68273b2c7ab5d46a68c3ad72ec42d3d6b8151645
Fedor Buryakov <58090653+kukusik2607ept@users.noreply.github.com>
2021-04-08 19:34:17 +0300

    Обновил шапку и меню, добавил основное содержимое
    Добавлен скрипт с помощью которого, можно скроллить превью видео

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const swiper_1 = new Swiper('.channel-slider-1', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next-1',
+    prevEl: '.channel-button-prev-1',
+  },
+
+});
+
+const swiper_2 = new Swiper('.channel-slider-2', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next-2',
+    prevEl: '.channel-button-prev-2',
+  },
+
+});
+
+const swiper_3 = new Swiper('.channel-slider-3', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next-3',
+    prevEl: '.channel-button-prev-3',
+  },
+
+});
